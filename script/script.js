7812d5507c2b8b237ab1a28644f129111fe9f46a
Usama-khanx <usamak2004@gmail.com>
2024-09-29 19:38:59 +0500

    changes a background color adds a media queries and some other changes in javaScript specially in age calculatin

--- script/script.js
+++ script/script.js
@@ -6,19 +6,37 @@ document.getElementById("calculateBtn").addEventListener('click', function(){
 
    var allYears = document.getElementById("yearsBtn");
    var allMonths = document.getElementById("monthsBtn");
-   var allDays = document.getElementById("daysBtn");
+   var allDays = document.getElementById("datesBtn");
 
    if(inputdate){
-    var mydate = new Date;
 
-    var yr = mydate.getFullYear()
-    var mn = mydate.getDate()
-    var dd =mydate.getDay()
+      
+      var birthDay = new Date(inputdate);
+      
+      var birthYear = birthDay.getFullYear();
+      var birthMonth = birthDay.getMonth();
+      var birthDate = birthDay.getDate();
+      
+      
+      var current = new Date();
+      
+      var currentYear = current.getFullYear();
+      var currentMonth = current.getMonth();
+      var currentDate = current.getDate();
+      
+      
+      var year = currentYear - birthYear;
+      var month = currentMonth - birthMonth;
+      var date = currentDate - birthDate;
 
-    allDays.innerHTML = `${dd} <br> Days`;
-allMonths.innerHTML = `${mn} <br> Months`;
-allYears.innerHTML = `${yr} <br> Years`;
+
+    
+
+      allYears.innerHTML = `${year}`;
+      allMonths.innerHTML = `${month}`;
+      allDays.innerHTML = `${date}`;
 
    }
+   
    
 })
