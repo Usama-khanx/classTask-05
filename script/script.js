6f60aa125d7b0990365df098f004e2d56146ecdf
Usama-khanx <usamak2004@gmail.com>
2024-09-29 10:43:44 +0500

    adds a break line in a button text

--- script/script.js
+++ script/script.js
@@ -15,9 +15,9 @@ document.getElementById("calculateBtn").addEventListener('click', function(){
     var mn = mydate.getDate()
     var dd =mydate.getDay()
 
-    allDays.textContent += dd;
-    allMonths.textContent += mn;
-    allYears.textContent += yr;
+    allDays.innerHTML = `${dd} <br> Days`;
+allMonths.innerHTML = `${mn} <br> Months`;
+allYears.innerHTML = `${yr} <br> Years`;
 
    }
    
