395e177a4a495b448b8b9767fa14abbd9b71e2fa
Usama-khanx <usamak2004@gmail.com>
2024-09-29 09:48:52 +0500

    adds a script file and connects to a to id

---
+++ script/script.js
@@ -0,0 +1,15 @@
+
+
+document.getElementById("calculateBtn").addEventListener('click', function(){
+
+    document.getElementById("inputDateField");
+
+    var AllYears = document.getElementById("yearsBtn");
+    var AllMonths = document.getElementById("monthsBtn");
+    var AllDays = document.getElementById("daysBtn");
+
+})
+
+function dateCalculation(){
+    
+}
